4069ad2c6682d1762d625455478ac87a549743b2
Sam Cater <sam@samcater.com>
2013-04-08 23:05:53 +0100

    Added basic test

--- pinfo.pl
+++ pinfo.pl
@@ -1,1 +1,7 @@
-file = /var/www/stats/index.html
+$file = "/var/www/stats/index.html";
+$local = "./data";
+`touch $local`;
+`echo '<title>Pi Info</title>' >> $local`;
+`echo 'Information' >> $local`;
+`sudo vcgencmd measure_temp >> $local`;
+`sudo mv $local $file`;
