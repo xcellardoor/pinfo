89c81e34a551255ac46685bac76cfc37aadf5e55
Sam Cater <sam@samcater.com>
2013-04-11 10:17:16 +0100

    temperature works

--- pinfo.pl
+++ pinfo.pl
@@ -1,7 +1,17 @@
+$num = 0;
 $file = "/var/www/stats/index.html";
 $local = "./data";
+sub temp(){
+print "ran but not loop";
+while(){
+print "ran";
 `touch $local`;
 `echo '<title>Pi Info</title>' >> $local`;
 `echo 'Information' >> $local`;
 `sudo vcgencmd measure_temp >> $local`;
 `sudo mv $local $file`;
+sleep(60);
+}
+}
+
+temp();
