03147120a81485a7dce5330b625bf12ea396ebda
Sam Cater <sam@samcater.com>
2013-05-05 20:42:54 +0100

    Update

--- pinfo.pl
+++ pinfo.pl
@@ -2,13 +2,14 @@ $num = 0;
 $file = "/var/www/stats/index.html";
 $local = "./data";
 sub temp(){
-print "ran but not loop";
 while(){
 print "ran";
 `touch $local`;
 `echo '<title>Pi Info</title>' >> $local`;
-`echo 'Information' >> $local`;
+`echo '<h2>Information<h2><br>' >> $local`;
 `sudo vcgencmd measure_temp >> $local`;
+`echo '<br>Process count:' >> $local`;
+`ps aux | wc -l >> $local`;
 `sudo mv $local $file`;
 sleep(60);
 }
